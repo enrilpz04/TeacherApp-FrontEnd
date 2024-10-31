a42c9eaf8fdd4cd3a9e6fb6523c9ad55d45c2434
mlpz <miguelkrasnob@gmail.com>
2024-10-31 23:43:28 +0100

    Creado formulario dummy de prueba de login conectado a user.service.ts

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  @Input() userForm: FormGroup;
+  constructor(private userService: UserService) {
+    this.userForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    });
+  }
 
+  login() {
+    this.userService.login(this.userForm.value['email'], this.userForm.value['password']).then(
+      (response: any) => {
+        console.log(response);
+        // Manejar la respuesta del login
+      },
+      (error: any) => {
+        console.error(error);
+      }
+    );
+  }
+
+  checkControl(formControlName: string, validator: string): boolean | undefined {
+    return this.userForm.get(formControlName)?.hasError(validator) && this.userForm.get(formControlName)?.touched;
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UserService {
+  login(email: string, password: string) : Promise<any> {
+    throw new Error('Method not implemented.');
+  }
 
   constructor() { }
 }
