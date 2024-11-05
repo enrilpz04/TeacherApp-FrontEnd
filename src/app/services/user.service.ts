7ad7f4d5825df5ef80db83732966e03e515f7321
mlpz <miguelkrasnob@gmail.com>
2024-11-05 18:17:14 +0100

    Modificado login post

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
 
 @Injectable({
@@ -12,7 +12,12 @@ export class UserService {
 
   login(email: string, password: string) : Promise<any> {
     console.log('Email: ' + email + ' - Contraseña: ' + password);
-    return lastValueFrom(this.httpClient.get<string>(`${this.apiUrl}/?email=${email}&password=${password}`));
+    let body = '{"email":"' + email + '", "password":"' + password + '"}';
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+     });
+    let options = { headers: headers };
+    return lastValueFrom(this.httpClient.post<string>(this.apiUrl + "/login", body, options));
   }
 
   constructor() { }
