446db86c02b86b9c3e1ba380f0417fa89ef7dd99
enrilpz04 <enriquelopezgordillo@gmail.com>
2024-11-15 12:09:30 +0100

    Get Teacher By Id from API

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
 
 @Injectable({
@@ -12,7 +12,17 @@ export class UserService {
 
   login(email: string, password: string) : Promise<any> {
     console.log('Email: ' + email + ' - Contraseña: ' + password);
-    return lastValueFrom(this.httpClient.get<string>(`${this.apiUrl}/?email=${email}&password=${password}`));
+    let body = '{"email":"' + email + '", "password":"' + password + '"}';
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+     });
+    let options = { headers: headers };
+    return lastValueFrom(this.httpClient.post<string>(this.apiUrl + "/login", body, options));
+  }
+
+  search(filtro1: string, filtro2: string) : Promise<any> {
+    console.log('filtro1: ' + filtro1 + ' - filtro2: ' + filtro2);
+    return lastValueFrom(this.httpClient.get<string>(`${this.apiUrl}/search?filtro1=${filtro1}&filtro2=${filtro2}`));
   }
 
   constructor() { }
