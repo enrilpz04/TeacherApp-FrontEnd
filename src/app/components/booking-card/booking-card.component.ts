3db43f2d47b17d44cd340ce43180a8de183244f9
mlpz2 <miguelkrasnob@gmail.com>
2024-11-29 11:09:02 +0100

    Components for booking created

---
+++ src/app/components/booking-card/booking-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './booking-card.component.html',
+  styleUrl: './booking-card.component.css'
+})
+export class BookingCardComponent {
+
+}
