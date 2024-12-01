1817cf04062949c775a5bda470a1f38084ffab67
enrilpz04 <enriquelopezgordillo@gmail.com>
2024-12-01 18:51:31 +0100

    new fun to bookings service. date + teacherId

--- src/app/services/bookings.service.ts
+++ src/app/services/bookings.service.ts
@@ -19,43 +19,51 @@ export class BookingsService {
 
   async getAllBookingsFromStudent(studentId: string): Promise<IBooking[]> {
     return firstValueFrom(this.http.get<any>(this.apiUrl + 'student/' + studentId))
-    .then(response => { return response; });
+      .then(response => { return response; });
   }
 
   async getAllBookingsFromTeacher(teacherId: string): Promise<IBooking[]> {
     return firstValueFrom(this.http.get<any>(this.apiUrl + 'teachers/' + teacherId))
-    .then(response => { return response; });
+      .then(response => { return response; });
+  }
+
+  async getBookingsByDateAndTeacherId(date: Date, teacherId: number): Promise<IBooking[]> {
+    const options = date && teacherId ?
+      { params: new HttpParams().set('date', date.toString()).set('teacherId', teacherId) } : {};
+    return firstValueFrom(this.http.get<any>(this.apiUrl + 'date', options)).then(response => {
+      return response;
+    })
   }
 
   async getAllBookingsBetweenStudentAndTeacher(studentId: string, teacherId: string): Promise<IBooking[]> {
-    const options = studentId && teacherId ? 
-    { params: new HttpParams().set('studentId', studentId).set('teacherId', teacherId) } : {};
+    const options = studentId && teacherId ?
+      { params: new HttpParams().set('studentId', studentId).set('teacherId', teacherId) } : {};
     return firstValueFrom(this.http.get<any>(this.apiUrl + 'between', options)).then(response => {
       return response;
     });
   }
-  
-  async createBooking(booking: IBooking) : Promise<IBooking> {
+
+  async createBooking(booking: IBooking): Promise<IBooking> {
     let headers = new HttpHeaders({
       'Content-Type': 'application/json'
-     });
+    });
     let options = { headers: headers };
     return firstValueFrom(this.http.post<any>(this.apiUrl, booking, options)).then(response => {
       return response;
     });
   }
-  
-  async updateBooking(booking: IBooking) : Promise<IBooking> {
+
+  async updateBooking(booking: IBooking): Promise<IBooking> {
     let headers = new HttpHeaders({
       'Content-Type': 'application/json'
-     });
+    });
     let options = { headers: headers };
     return firstValueFrom(this.http.put<any>(this.apiUrl + booking.id, booking, options)).then(response => {
       return response;
     });
   }
-  
-  async deleteBooking(id: string) : Promise<IBooking> {
+
+  async deleteBooking(id: string): Promise<IBooking> {
     return firstValueFrom(this.http.delete<any>(this.apiUrl + id)).then(response => {
       return response;
     });
