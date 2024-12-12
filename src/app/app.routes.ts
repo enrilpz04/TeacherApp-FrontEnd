7662accd44c335d3fe4433ccb22f306be4c5b5d3
enrilpz04 <enriquelopezgordillo@gmail.com>
2024-12-12 22:02:11 +0100

    Initialization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,7 @@ import { MessagesComponent } from './pages/dashboard/messages/messages.component
 import { NotificationsComponent } from './pages/dashboard/notifications/notifications.component';
 import { ClassesComponent } from './pages/dashboard/classes/classes.component';
 import { MapsComponent } from './components/maps/maps.component';
+import { AdminPanelComponent } from './pages/dashboard/admin-panel/admin-panel.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -27,6 +28,7 @@ export const routes: Routes = [
     { path: 'dashboard-home', component: DashboardHomeComponent },
     { path: 'messages', component: MessagesComponent },
     { path: 'notifications', component: NotificationsComponent },
-    { path: 'classes', component: ClassesComponent }
+    { path: 'classes', component: ClassesComponent },
+    { path: 'admin', component: AdminPanelComponent }
   ]}
 ];
